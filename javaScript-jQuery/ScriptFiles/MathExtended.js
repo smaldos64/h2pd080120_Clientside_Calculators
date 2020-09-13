3b867e99b9f44fc12c4050e6f3f9bb33a811169e
smaldos64 <gudumholm34@gmail.com>
2020-09-13 13:58:13 +0200

    Funktionalitet i javaScript lommeregneren er nu stort set den samme som den ndbyggede lommeregner i Windows 10.

--- javaScript-jQuery/ScriptFiles/MathExtended.js
+++ javaScript-jQuery/ScriptFiles/MathExtended.js
@@ -2,6 +2,9 @@
 
 Counter = 0;
 MathOperationValue = -1;
+//PreviousResultIsShownInDisplay = false;
+const CalculatorModeEnum = Object.freeze({ "NewCalculation": 1, "CalculationOnPrevioiusResult": 2 });
+let CalculatorMode = CalculatorModeEnum.NewCalculation;
 
 CalculatorLine1 = null;
 CalculatorLine2 = null;
@@ -42,7 +45,7 @@ function MakeMathOperationKeys() {
 
     TextHolder = '=';
     MathArray.push({
-        Text: TextHolder, ID: "btnEqual", OperatorValue: Counter++, BackgroundColor: "blue", TextColor: "white", MathOperation: function () { ClrDisplayAndStatus() }
+        Text: TextHolder, ID: "btnEqual", OperatorValue: Counter++, BackgroundColor: "blue", TextColor: "white", MathOperation: "CalcMathResult()", MathOperationCalculate: null
     });
 
     TextHolder = '+';
@@ -120,8 +123,6 @@ function AddButtonsToPage() {
 }
 
 function ClrDisplayAndStatus() {
-    //TextBoxElements = document.getElementsByName("txtCalculatorLine");
-
     Counter = 0;
     while (Counter < TextBoxElements.length) {
         TextBoxElements[Counter].value = "";
@@ -130,6 +131,11 @@ function ClrDisplayAndStatus() {
 }
 
 function AddNumericCharacterToTextBox(argument) {
+    if (CalculatorModeEnum.CalculationOnPrevioiusResult == CalculatorMode) {
+        CalculatorMode = CalculatorModeEnum.NewCalculation;
+        ClrDisplayAndStatus();
+    }
+
     if ('±' == argument) {
         if (CalculatorLine2.value.length > 0) {
             if (CalculatorLine2.value.charAt(0) != '-') {
@@ -161,29 +167,44 @@ function AddNumericCharacterToTextBox(argument) {
     CalculatorLine2.value = CalculatorLine2.value + argument.toString();
 }
 
+function GetMathStringFromCalculatorLine1(CalculatorLine1Here) {
+    var SpacePosition = CalculatorLine1Here.search(' ');
+    if (SpacePosition > 0) {
+        CalculatorLine1Here = CalculatorLine1Here.substring(0, SpacePosition);
+        return CalculatorLine1Here;
+    }
+}
+
 function SetMathOperatorValue(Value, ValueText) {
     if ((0 != CalculatorLine2.value.length) || (0 != CalculatorLine1.value.length) ) {
         MathOperationValue = Value;
-        if (0 == CalculatorLine1.value.length) {
+        if (CalculatorModeEnum.CalculationOnPrevioiusResult == CalculatorMode) {
             CalculatorLine1.value = CalculatorLine2.value + " " + ValueText;
-            CalculatorLine2.value = "";
         }
         else {
-            var SpacePosition = CalculatorLine1.value.search(' ');
-            if (SpacePosition > 0) {
-                CalculatorLine1.value = CalculatorLine1.value.substring(0, SpacePosition);
-                CalculatorLine1.value += " " + ValueText;
+            if (0 == CalculatorLine1.value.length) {
+                CalculatorLine1.value = CalculatorLine2.value + " " + ValueText;
+                CalculatorLine2.value = "";
+            }
+            else {
+                var SpacePosition = CalculatorLine1.value.search(' ');
+                if (SpacePosition > 0) {
+                    CalculatorLine1.value = CalculatorLine1.value.substring(0, SpacePosition);
+                    CalculatorLine1.value += " " + ValueText;
+                }
             }
         }
-        //document.getElementById("txtCalculatorLine1").value = document.getElementById("txtCalculatorLine1").value + " " + ValueText;
     }
 }
 
 function CalcMathResult() {
-    MathOperationResult = CalculateMathResult(MathOperationValue,
-        document.getElementById("txtCalculatorLine1").value,
-        document.getElementById("txtCalculatorLine2").value);
-    document.getElementById("txtResult").value = MathOperationResult;
+    if ((0 != CalculatorLine1.value.length) && (0 != CalculatorLine2.value.length)) {
+        MathOperationResult = CalculateMathResult(MathOperationValue,
+            GetMathStringFromCalculatorLine1(CalculatorLine1.value),
+            CalculatorLine2.value);
+        //ClrDisplayAndStatus();
+        CalculatorLine2.value = MathOperationResult;
+    }
 }
 
 function CalculateMathResult(MathOperation, Value1String, Value2String) {
@@ -193,7 +214,9 @@ function CalculateMathResult(MathOperation, Value1String, Value2String) {
     while ((Counter < MathArray.length) && (false == MathOperationFound)) {
         if (MathArray[Counter].OperatorValue == MathOperation) {
             MathOperationFound = true;
-            return (MathArray[Counter].MathOperation(parseFloat(Value1String), parseFloat(Value2String)).toString());
+            CalculatorLine1.value = Value1String + " " + MathArray[Counter].Text + " " + Value2String + " " + "="
+            CalculatorMode = CalculatorModeEnum.CalculationOnPrevioiusResult;
+            return (MathArray[Counter].MathOperationCalculate(parseFloat(Value1String), parseFloat(Value2String)).toString());
         }
         else {
             Counter++;
